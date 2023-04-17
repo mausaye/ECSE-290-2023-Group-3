922ff3828315b8367579e945b887f606725be9dd
Robbie Hammond <robbiehammond3@gmail.com>
2023-04-17 15:09:51 -0400

    added RPC howto

--- Assets/Scripts/Scoreboard/RPC.cs
+++ Assets/Scripts/Scoreboard/RPC.cs
@@ -3,19 +3,30 @@ using UnityEngine.Networking;
 using System.Collections;
 using System.Collections.Generic;
 
-public struct ScoreMessage {
-    public int score;
-}
+/*
+    HOW TO USE: PLEASE READ!
+
+        - If the scene needs to connect to the scoreboard in any way, 
+          go to whatever file that you want to call this script from (So like Scoreboard.cs),
+          add a field "private RPC rpc;", and then in the Start() function,
+          add this line: "rpc = this.gameObject.AddComponent(typeof(RPC)) as RPC;".
+          Now, if you want to upload a score, do "rpc.uploadTime(time)". To retrieve 
+          the top 5 scores, do "rpc.getTimes()". 
+
+          IMPORTANT NOTE: It will take about 1 second from when you initialize the RPC 
+          and for the the scores to be downloaded from the server. Make sure you structure your
+          code such that the initialization of the RPC happens a good bit before you need to display scores.
+
+          Note that the same concept applies for uploading. It'll take about 1 second for the scores to download 
+          from when you call uploadTime(score). As stated, structure the code such that there's some level
+          of delay between when you call uploadTime(score) and when you actually need to display the updated times.
+*/
+
+
+
 public class RPC : MonoBehaviour
 {
 
-    private string returnedScoreboard = "UNASSIGNED";
-
-    void Start() 
-    {
-        // Just so we have something to always display
-        fetchInitialScoreboardData();
-    }
 
     // Send over game time in seconds, also assigns times with updated scoreboard.
     public void uploadTime(int time) {
@@ -24,17 +35,39 @@ public class RPC : MonoBehaviour
         StartCoroutine(PostRequest("http://3.19.53.71:5050/", JsonUtility.ToJson(m)));
     }
 
-    public void fetchInitialScoreboardData() {
+    // Return the times as a string in the format [time1, time2, time3, etc]. Sorted in descending order.
+    public string getTimes() {
+        return returnedScoreboard;
+    }
+
+    void Start() 
+    {
+        // Just so we have something to always display, fetch on initialization.
+        fetchInitialScoreboardData();
+    }
+
+
+
+
+
+
+
+    /* -- Internal stuff. Don't worry about this. -- */
+
+    // Converting classes to JSON is much simpler than converting strings in Unity. Pretty stupid.
+    private struct ScoreMessage {
+        public int score;
+    }
+
+    private string returnedScoreboard = "UNASSIGNED";
+
+    private void fetchInitialScoreboardData() {
         if (returnedScoreboard == "UNASSIGNED") {
             StartCoroutine(GetRequest("http://3.19.53.71:5050/"));
         }
     }
 
-    public string getTimes() {
-        return returnedScoreboard;
-    }
-
-    //from Unity docs
+    //edited from Unity docs.
     private IEnumerator GetRequest(string uri) {
         using (UnityWebRequest webRequest = UnityWebRequest.Get(uri)) {
 
@@ -58,6 +91,8 @@ public class RPC : MonoBehaviour
             }
         }
     }
+
+    //also edited from Unity docs.
     private IEnumerator PostRequest(string url, string json)
     {
         var uwr = new UnityWebRequest(url, "POST");
@@ -75,7 +110,6 @@ public class RPC : MonoBehaviour
         }
         else
         {
-
             this.returnedScoreboard = uwr.downloadHandler.text;
             Debug.Log("Received: " + uwr.downloadHandler.text);
         }
