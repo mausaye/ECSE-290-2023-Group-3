47907a4a9d9bcc4aafd0963a9e5f6084163d5c99
Robbie Hammond <robbiehammond3@gmail.com>
2023-04-23 17:16:39 -0400

    readded basic textbox functionality

--- Assets/Scripts/Utils/TextboxManager.cs
+++ Assets/Scripts/Utils/TextboxManager.cs
@@ -1,15 +1,19 @@
 using UnityEngine;
+using TMPro;
 
-public class TextboxManager : MonoBehaviour {
-
-    public static GameObject textbox;
+public class TextboxManager : MonoBehaviour 
+{
+    [SerializeField] private static GameObject textbox;
+    [SerializeField] private static TextMeshProUGUI dialogueText;
 
     void Start() {
-
+        textbox = GameObject.FindWithTag("Textbox");
+        dialogueText = GetComponent<TextMeshProUGUI>();
+        textbox.transform.localScale = new Vector3(0, 0, 0);
     }
 
     public static void createTextbox(string initialText) {
-
+        textbox.transform.localScale = new Vector3((3 * (Screen.width / ((RectTransform)textbox.transform).rect.width)) / 4, (Screen.height / ((RectTransform)textbox.transform).rect.height) / 7, 1.0f);
     }
 
     public static void setText(string newText) {
@@ -17,6 +21,7 @@ public class TextboxManager : MonoBehaviour {
     }
 
     public static void removeTextbox() {
+        textbox.transform.localScale = new Vector3(0, 0, 0);
 
     }
 }
